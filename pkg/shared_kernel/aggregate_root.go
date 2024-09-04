9c75e84a72056aba62c38c044746596ce22a207e
a123 <wangjun@simbalink.cn>
2024-09-04 15:50:19 +0800

    绑定事件时同时去重

--- pkg/shared_kernel/aggregate_root.go
+++ pkg/shared_kernel/aggregate_root.go
@@ -4,7 +4,19 @@ type AggregateRoot struct {
 	domainEvents []DomainEvent
 }
 
+// ApplyDomain
+//
+//	@Description: 绑定事件，需要去重
+//	@receiver ar
+//	@param e
 func (ar *AggregateRoot) ApplyDomain(e DomainEvent) {
+	// 直接操作原始切片 ar.domainEvents
+	for i, v := range ar.domainEvents {
+		if v.Identity() == e.Identity() {
+			ar.domainEvents[i] = e
+			return
+		}
+	}
 	ar.domainEvents = append(ar.domainEvents, e)
 }
 
