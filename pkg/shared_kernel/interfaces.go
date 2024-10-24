818e4d7d14d46ecd90d3006280197f832d6ece2f
liuao <liuao@simbalink.cn>
2024-10-24 17:19:16 +0800

    增加聚合事件，事件处理器

--- pkg/shared_kernel/interfaces.go
+++ pkg/shared_kernel/interfaces.go
@@ -1,12 +1,16 @@
 package sharedkernel
 
 import (
+	"context"
 	"time"
 )
 
 type (
+	Handler     func(context.Context, DomainEvent) (interface{}, error)
 	DomainEvent interface {
 		CreateAt() time.Time
 		Identity() string
+		InitHandler(Handler)
+		Handle(ctx context.Context) (interface{}, error)
 	}
 )
