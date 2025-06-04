e06fe1f378b756461e25d09de232a842dd88b0ac
liuao <liuao@simbalink.cn>
2025-06-04 11:29:11 +0800

    InitHandler(...Handler)   可变参数

--- pkg/shared_kernel/interfaces.go
+++ pkg/shared_kernel/interfaces.go
@@ -10,7 +10,7 @@ type (
 	DomainEvent interface {
 		CreateAt() time.Time
 		Identity() string
-		InitHandler(Handler)                             // 初始化事件处理函数
+		InitHandler(...Handler)                          // 初始化事件处理函数
 		Handle(ctx context.Context) (interface{}, error) // 调用事件处理函数
 		IgnoreErr() bool                                 // 是否忽略事件处理错误
 	}
